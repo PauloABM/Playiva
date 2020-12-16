2915216d87f879ee7a90dc7187d94cd999c8a975
Nayara Soares <42041514+nayaras@users.noreply.github.com>
2020-12-16 01:06:51 -0300

    front cadastro (incompleto)

---
+++ pages/user/cadastro.content.php
@@ -0,0 +1,43 @@
+<section class="section">
+    <div class="columns">
+
+        <div class="column is-7 is-offset-2">
+            <h1 class="title">Cadastro</h1>
+            <div class="field">
+                <label class="label">Nome</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Nome" required>
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Sobrenome</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Sobrenome" required>
+                </div>
+            </div>
+            <div class="field">
+                <label for="" class="label">Email</label>
+                <div class="control has-icons-left">
+                    <input type="email" placeholder="ex. ana@gmail.com" class="input" required>
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-envelope"></i>
+                    </span>
+                </div>
+            </div>
+            <div class="field">
+                <label for="" class="label">Senha</label>
+                <div class="control has-icons-left">
+                    <input type="password" placeholder="*******" class="input" required>
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-lock"></i>
+                    </span>
+                </div>
+            </div>
+            <div class="field is-grouped column is-pulled-right">
+                <div class="control">
+                    <button class="button is-link">Cadastrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
