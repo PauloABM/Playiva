e4903aa2fc9a6204d3c64d92b5b561bfab7c101f
Nayara Soares <42041514+nayaras@users.noreply.github.com>
2020-12-16 22:45:56 -0300

    altera caminho main para Iarah

--- pages/main/main.php
+++ pages/main/main.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../../config/db.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/class/AutoLoader.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Playiva/class/AutoLoader.class.php';
 
 $prefix = basename($_SERVER['SCRIPT_FILENAME'], '.php');
 $css = file_exists(realpath(__DIR__) . '/' . $prefix . '.styles.css') ? realpath(__DIR__) . '\\' . $prefix . '.styles.css' : false;
